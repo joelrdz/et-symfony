97bc0dc42a728bd4c54e73f97ba09ee415aa7027
Joel Rdz <joelrdzh@gmail.com>
2023-08-22 23:24:57 -0600

    Displaying received data (url) in twig template

--- src/Controller/StandardController.php
+++ src/Controller/StandardController.php
@@ -14,6 +14,7 @@ class StandardController extends AbstractController
         dump($id);
         return $this->render('standard/index.html.twig', [
             'controller_name' => 'StandardController',
+            'route_id' => $id,
         ]);
     }
 }
